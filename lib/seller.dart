e38f9504c928ec480e4412d4fda10e0b0012ed1d
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-01-21 00:52:48 +0545

    add the data to firebase by salemodel

--- lib/seller.dart
+++ lib/seller.dart
@@ -1,10 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fireflutter/sellers_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-
+import 'package:fireflutter/models/sellmodels.dart';
 
 class BuyerSellerScreen extends StatefulWidget {
   @override
@@ -20,7 +21,7 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
   final List<Marker> _markers = [
     const Marker(
       point: LatLng(51.509364, -0.128928), // Example: Kathmandu
-      child:  Icon(
+      child: Icon(
         Icons.location_pin,
         color: Colors.red,
         size: 40,
@@ -33,9 +34,8 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
         color: Colors.red,
         size: 40,
       ),
-    ),  
+    ),
   ];
-  
 
   @override
   void dispose() {
@@ -103,7 +103,7 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
             // Submit Button
             Center(
               child: ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   if (_weightController.text.isEmpty ||
                       _addressController.text.isEmpty ||
                       _phoneController.text.isEmpty) {
@@ -113,6 +113,18 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
                     return;
                   }
 
+                  /// create new object of model (making sure kun field ma kasto value janxa - controller le dekhayejsto)
+                  final newsale = sellmodel(
+                      id: DateTime.now().toString(),
+                      weight: _weightController.text,
+                      address: _addressController.text,
+                      phonenumber: _phoneController.text);
+                  
+                  //add data to the firebase
+                  await FirebaseFirestore.instance
+                      .collection('salesdetails')
+                      .add(newsale.toJson());
+
                   print('Weight: ${_weightController.text} kg');
                   print('Address: ${_addressController.text}');
                   print('Phone: ${_phoneController.text}');
@@ -120,6 +132,11 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
                     SnackBar(
                         content: Text('Your details have been submitted.')),
                   );
+                  
+                  //clearing the textfields
+                  _weightController.clear();
+                  _addressController.clear();
+                  _phoneController.clear();
                 },
                 child: Text('Submit'),
               ),
@@ -134,11 +151,16 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
                 IconButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const SellerMap()));
-                      print('Icon btn to see full screen location pressed');
-                    },
-                    icon: const Icon(Icons.arrow_right),iconSize: 30,)
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const SellerMap()));
+                    print('Icon btn to see full screen location pressed');
+                  },
+                  icon: const Icon(Icons.arrow_right),
+                  iconSize: 30,
+                )
               ],
             ),
             const SizedBox(height: 16),
@@ -152,7 +174,6 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
                       51.509364, -0.128928), // Center the map over London
                   initialZoom: 9.2,
                 ),
-                
                 children: [
                   TileLayer(
                     // Display map tiles from any source
@@ -172,7 +193,7 @@ class _BuyerSellerScreenState extends State<BuyerSellerScreen> {
                       // Also add images...
                     ],
                   ),
-                  MarkerLayer(markers: _markers), 
+                  MarkerLayer(markers: _markers),
                 ],
               ),
             ),
