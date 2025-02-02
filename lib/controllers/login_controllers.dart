21eb68651b435157d56a246e6de85c6149dc633f
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-02-02 11:55:40 +0545

    add login controller

---
+++ lib/controllers/login_controllers.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fireflutter/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+
+class LoginController {
+
+  static Future<void> login( {required BuildContext context,required email, required password}) async{
+    try{
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    print("Account created");
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return SplashScreen();
+        },
+      ),(route){
+        return false;   
+      }
+    );
+    }
+    catch(e){
+      SnackBar snackbarmessage = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackbarmessage);  
+    }
+  }
+}
