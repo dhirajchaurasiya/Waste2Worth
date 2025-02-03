7686e7eaeeb52dfeb0b84c5f71c0837fb48435db
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-02-03 18:48:11 +0545

    add homescreen with navbar

---
+++ lib/widgets/homescreen.dart
@@ -0,0 +1,66 @@
+import 'package:fireflutter/screens/dashboard.dart';
+import 'package:fireflutter/screens/profile.dart';
+import 'package:fireflutter/screens/seller.dart';
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
+
+  final List<Widget> _pages = [
+    DashboardScreen(),
+    BuyerSellerScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Waste2Worth'),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.woo_commerce),
+            label: 'Buy/Sell',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.green[100],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
