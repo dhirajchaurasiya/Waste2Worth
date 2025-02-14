4e3a8901fd3223ef9fe731002a511e5d7d29af5f
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-02-14 17:09:40 +0545

    add logout option and fromat user avatar

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fireflutter/screens/edit_profile.dart';
+import 'package:fireflutter/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/userprovider.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -8,15 +15,78 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+
+  // Map<String, dynamic>? userdata = {};
+
+  // var db = FirebaseFirestore.instance;
+  // var authUser = FirebaseAuth.instance.currentUser;
+
+  // void getdata(){
+  //   db.collection("users").doc(authUser!.uid).get().then((value) {
+  //     userdata = value.data();
+  //     setState(() {
+        
+  //     });
+  //   });
+  // }
+
+  // @override
+  // void initState() {
+  //   getdata();
+  //   // TODO: implement initState
+  //   super.initState();
+  // }
+
   @override
   Widget build(BuildContext context) {
+ 
+    var userprovider = Provider.of<Userprovider>(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              child: Text(userprovider.userName[0], style: TextStyle(fontSize: 30),),
+              radius: 50,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            //displaying the fetched data
+            Text(
+              userprovider.userName,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            
+            TextButton(onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context){return SplashScreen();},), (route) => false);
+            }, child: Text('Logout')),
+
+            Text(userprovider.userEmail),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return EditProfile();
+                      },
+                    ),
+                  );
+                  print(userprovider.userName);
+                },
+                child: Text("Edit profile")),
+
+            
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('Profile Screen'),
-      )
     );
   }
 }
