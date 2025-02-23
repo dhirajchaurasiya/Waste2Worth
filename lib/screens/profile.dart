cc049da6717e46a68debb5b977610518d1c64ffd
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-02-24 00:22:32 +0545

    modify profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,67 +1,141 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:fireflutter/screens/edit_profile.dart';
+import 'package:fireflutter/providers/userprovider.dart';
 import 'package:fireflutter/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
+
+import 'package:fireflutter/screens/edit_profile.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/userprovider.dart';
-
-class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
-
+class ProfilePage extends StatefulWidget {
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfilePageState extends State<ProfilePage> {
+  void logout() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => SplashScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
- 
     var userprovider = Provider.of<Userprovider>(context);
 
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              child: Text(userprovider.userName[0], style: TextStyle(fontSize: 30),),
-              radius: 50,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            //displaying the fetched data
-            Text(
-              userprovider.userName,
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            
-            TextButton(onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context){return SplashScreen();},), (route) => false);
-            }, child: Text('Logout')),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.green,
+                          Colors.green[200]!,
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          child: Text(userprovider.userName[0]),
+                        ),
+                        SizedBox(width: 16),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${userprovider.userName}',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Seller Account',
+                              style: TextStyle(
+                                color: Colors.grey[100],
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              buildSectionTitle('Selling'),
+              buildListTile(Icons.mail, '${userprovider.userEmail}'),
+              buildListTile(Icons.category, 'CategoriesSelected '),
+              buildSectionTitle('Settings'),
+              buildListTile(Icons.person, 'My Profile'),
+              buildListTile(Icons.settings, 'Logout'),
+              buildSectionTitle('Resources'),
+              buildListTile(Icons.info, 'About Us'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-            Text(userprovider.userEmail),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.green, foregroundColor: Colors.white),
-                onPressed: () {
+  Widget buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Text(
+          title,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildListTile(IconData icon, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightGreen[100],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: ListTile(
+          leading: Icon(icon, color: Colors.black),
+          title: Text(
+            title,
+            style: TextStyle(color: Colors.black),
+          ),
+          trailing: GestureDetector(
+              onTap: () {
+                if (title == 'My Profile') {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return EditProfile();
-                      },
-                    ),
+                    MaterialPageRoute(builder: (context) => EditProfile()),
                   );
-                },
-                child: Text("Edit profile")),
-
-            
-          ],
+                }
+                if (title == 'Logout') {
+                  logout();
+                }
+              },
+              child: Icon(Icons.chevron_right, color: Colors.black)),
         ),
       ),
     );
