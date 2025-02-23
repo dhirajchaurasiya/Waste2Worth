09a7d0744bcd8d6e05d856245fa446d01866d98f
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-02-23 20:57:55 +0545

    add detail parameters

--- lib/models/sellmodels.dart
+++ lib/models/sellmodels.dart
@@ -1,28 +1,46 @@
 class sellmodel {
+  String? name;
+  String? email;
   String? id;
+  double? lat;
+  double? long;
   String? weight;
-  String? address;
   String? phonenumber;
 
-  sellmodel({this.id, this.weight, this.address, this.phonenumber});
+  sellmodel(
+      {this.id,
+      this.weight,
+      this.phonenumber,
+      this.lat,
+      this.long,
+      this.name,
+      this.email});
 
   sellmodel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     weight = json['weight'];
-    address = json['address'];
+    lat = json['lat'];
+    long = json['long'];
+    // address = json['address'];
+    name = json['name'];
+    email = json['email'];
     phonenumber = json['phonenumber'];
   }
 
   Map<String, dynamic> toJson() => {
-    // final Map<String, dynamic> data = new Map<String, dynamic>();
-    // ['weight'] = weight;
-    // ['address'] = address;
-    // ['phonenumber'] = phonenumber;
-    // return data;
+        // final Map<String, dynamic> data = new Map<String, dynamic>();
+        // ['weight'] = weight;
+        // ['address'] = address;
+        // ['phonenumber'] = phonenumber;
+        // return data;
 
-    'id': id,
-    'weight':weight,
-    'address':address,
-    'phonenumber':phonenumber
-  };
+        'id': id,
+        'weight': weight,
+        'lat': lat,
+        'long': long,
+        // 'address': address,
+        'name': name,
+        'email': email,
+        'phonenumber': phonenumber
+      };
 }
