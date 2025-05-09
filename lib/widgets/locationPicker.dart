c0d706b3f083ac8ce4c9bdb2b7c9757b52439506
dhirajchaurasiya10 <starkjon224@gmail.com>
2025-05-09 14:58:53 +0545

    add current location as default location to pick a location

--- lib/widgets/locationPicker.dart
+++ lib/widgets/locationPicker.dart
@@ -10,6 +10,36 @@ class LocationPicker extends StatefulWidget {
 
 class _LocationPickerState extends State<LocationPicker> {
   LatLng _pickedLocation = LatLng(27.7172, 85.3240); // Default to Kathmandu
+  Position? currentLocation;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+        forceAndroidLocationManager: true,
+      );
+
+      setState(() {
+        currentLocation = position;
+        _isLoading = false;
+      });
+
+      print(
+          "Current location: ${currentLocation!.latitude}, ${currentLocation!.longitude}");
+    } catch (e) {
+      print("Error fetching location: $e");
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,37 +47,43 @@ class _LocationPickerState extends State<LocationPicker> {
       appBar: AppBar(
         title: Text('Pick a Location'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          initialCenter: _pickedLocation,
-          initialZoom: 13.0,
-          onTap: (tapPosition, point) {
-            setState(() {
-              _pickedLocation = point;
-            });
-          },
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.fireflutter',
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                width: 80.0,
-                height: 80.0,
-                point: _pickedLocation,
-                child: const Icon(
-                  Icons.location_on,
-                  color: Colors.red,
-                  size: 48.0,
-                ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : FlutterMap(
+              options: MapOptions(
+                initialCenter: currentLocation != null
+                    ? LatLng(
+                        currentLocation!.latitude, currentLocation!.longitude)
+                    : LatLng(27.7172, 85.3240),
+                initialZoom: 13.0,
+                onTap: (tapPosition, point) {
+                  setState(() {
+                    _pickedLocation = point;
+                  });
+                },
               ),
-            ],
-          ),
-        ],
-      ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.fireflutter',
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(currentLocation!.latitude,
+                          currentLocation!.longitude),
+                      child: const Icon(
+                        Icons.location_on,
+                        color: Colors.red,
+                        size: 48.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context, _pickedLocation);
